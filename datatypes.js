d38b9c0be161f5dc47806030c0a3014cd58bb0a0
Rutuja Wayase <64628671+RutujaWayase@users.noreply.github.com>
2024-07-05 09:43:46 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+let a = "John"
+let b = 6
+console.log(a+b)
+
+console.log(typeof(a+b))
+
+const a1 = {
+    name: "Rutuja",
+    section: 1,
+    isPrincipal: false
+}
+
+//a1 = 45 //will throw an error as a1 is constant value can't be changed
+a1['friend'] = "Gail"
+a1['name'] = "Toylor"
+console.log(a1)
+
+const dict = {
+    appreciate: "recognize",
+    ataraxia: "a state of freedom",
+    yakka: "hardwork"
+}
+
+console.log(dict.yakka)
