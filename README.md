9d09ce77fc30cf3622fd218f527fbb0dadf13194
RutujaWayase <rutujawayase1234@gmail.com>
2025-02-11 23:51:26 +0530

    adding basic

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # JavaScript
 JS
+
+Variables: let var const
+1) we can not redeclare variable with let keyword but possible with var
+
+2) reassigning is allowed with let
+
+3) let => reassigning allowed. var => redeclaring and reassigning both are allowed.
+
+
+
---
+++ basic.js
@@ -0,0 +1,29 @@
+console.log("Hello world")
+
+let a=4
+//var let const
+console.log(a)
+
+console.log(typeof(a))
+
+
+let b = 234.6
+console.log(typeof(b))
+
+let c = "Rutuja"
+//var c = "Rutuja"
+console.log(typeof(c))
+
+let required = true
+console.log(typeof(required))
+// null and undefined => datatypes
+
+//let c = a+b (it did not work we can not redeclare variable with let keyword but possible with var)
+//var c = a+b
+c = a+b //reassigning is allowed with let
+//var c=a+b //this is also allowed
+console.log(c)
+//we can not redeclare variable with let keyword but possible with var
+console.log(required)
+
+
